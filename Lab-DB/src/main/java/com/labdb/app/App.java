c388eb2f885f3aa476d57e6120471a9d8f3d03a1
ElrohirGT <elrohirgt@gmail.com>
2025-02-13 16:25:26 -0600

    feat: Added credentials

--- Lab-DB/src/main/java/com/labdb/app/App.java
+++ Lab-DB/src/main/java/com/labdb/app/App.java
@@ -1,10 +1,20 @@
 package com.labdb.app;
 
-/**
- * Hello world!
- */
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String... args) {
+
+        // URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+        final String dbUri = "neo4j+s://4b79cc40.databases.neo4j.io";
+        final String dbUser = "neo4j";
+        final String dbPassword = "w2x6GKnBbaGXq8xMUO-Jxm_lQfYWYATgImJRrXIfCs8";
+
+        try (var driver = GraphDatabase.driver(dbUri, AuthTokens.basic(dbUser, dbPassword))) {
+            driver.verifyConnectivity();
+            System.out.println("Connection established.");
+        }
     }
 }
